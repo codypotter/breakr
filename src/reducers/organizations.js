b70da7768604ffdeb2e6a2b4427061cec9fefe24
enjoipotter <enjoipotter@gmail.com>
2019-02-28 12:17:25 -0800

    added reducer and action generators

--- src/reducers/organizations.js
+++ src/reducers/organizations.js
@@ -7,3 +7,57 @@ const demoOrganizations = {
   },
   allIds: ['organization1234567890']
 };
+
+/**
+ * Add Organization Action Generator
+ * Returns an action with data to add a new organization to the store
+ * @param {object} payload includes the id and name of the organization 
+ */
+const addOrganization = ({
+  id,
+  name,
+}) => ({
+  type: 'ADD_ORGANIZATION',
+  id,
+  name,
+});
+
+/**
+ * Clear Organizations Action Generator
+ * Returns an action with data to clear the organizations list
+ */
+const clearOrganizations = () => ({
+  type: 'CLEAR_ORGANIZATIONS'
+});
+
+/**
+ * Employee Category Reducer
+ * Interacts with employee categories in the store
+ */
+const employeeCategoriesStore = createStore((state = {}, action) => {
+  switch (action.type) {
+    case 'ADD_ORGANIZATION':
+      const {
+        id,
+        name
+      } = action;
+      return {
+        ...state,
+        byId: {
+          ...state.byId,
+          [id]: {
+            id,
+            name,
+          }
+        },
+        allIds: [
+          ...state.allIds,
+          id
+        ]
+      };
+    case 'CLEAR_ORGANIZATIONS':
+      return {};
+    default:
+      return state;
+  }
+});
