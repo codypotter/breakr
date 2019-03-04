773748b98c372db6cfd46d9a609eea6c836ed9b8
enjoipotter <enjoipotter@gmail.com>
2019-03-03 23:59:00 -0800

    added documentation and refactored

--- src/actions/notes.js
+++ src/actions/notes.js
@@ -1,20 +1,24 @@
 
 /**
- * Add Note Action Generator
+ * Add Note Action Generator -
  * Returns an action with data to add a new note to the store
- * @param {object} payload includes the id and text of a new note 
+ * @param {*}       payload -       the data to add a new note
+ * @param {string}  payload.id -    the id of the new note 
+ * @param {string}  payload.text -  the text of a new note 
  */
-export const addNote = ({ id, text }) => ({
+export const addNote = ({ id, text='' }) => ({
   type: 'ADD_NOTE',
   id,
   text
 });
 
 /**
- * Edit Note Action Generator
+ * Edit Note Action Generator -
  * Returns an action with data to edit a note in the store
- * @param {string} id the id of the note to edit
- * @param {Object} updates the relevant data of the note to update
+ * @param {string}  id -       the id of the note to edit
+ * @param {*}       updates -  the data of the note to update
+ * @example
+ *  editNote('note1234567890', { text: 'heres some new text' });
  */
 export const editNote = (id, updates) => ({
   type: 'EDIT_NOTE',
@@ -23,11 +27,11 @@ export const editNote = (id, updates) => ({
 });
 
 /**
- * Remove Note Action Generator
+ * Remove Note Action Generator -
  * Returns an action with data to remove a note from the store
  * @param {Object} payload includes the id of the note to remove 
  */
-export const removeNote = ({ id }) => ({
+export const removeNote = (id) => ({
   type: 'REMOVE_NOTE',
   id
 });
